7a998759a3e8c8763c8042227796c6f2e6c07d60
Raj Kumar <interplaytheheads@gmail.com>
2020-09-30 18:37:30 +0200

    Header component called

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      Hello Template
+      <Header />
 
     </div>
   );
