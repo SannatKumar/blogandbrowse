03ef3c70c3c5e5655db3ff68ec4faa55ac8aa5e6
Raj Kumar <interplaytheheads@gmail.com>
2020-10-01 17:05:45 +0200

    Form label created

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
+function CreateUser() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [address, setAddress] = useState("");
+  function makeUser() {
+    let data = { name, age, address };
+    console.warn("called", data);
+  }
 
-class CreateUser extends Component {
- state = {}
- render() {
-  return(
-   <div></div>
-    )
-   }
- }
-
+  return (
+    <div>
+      <h1> Make a new User</h1>
+      Name:  
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        name="username"
+        value={name}
+        required
+      />
+      <br />
+      <br />
+      Age:
+      <input type="text" onChange={(e) => setAge(e.target.value)} required />
+      <br /> <br />
+      Address: 
+      <input
+        type="text"
+        name="address"
+        onChange={(e) => setAddress(e.target.value)}
+        value={address}
+      />
+      <br /> <br />
+      <button onClick={makeUser}>Create User</button>
+    </div>
+  );
+}
 
 export default CreateUser;
